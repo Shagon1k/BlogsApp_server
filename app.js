e506dac1137292afddfd3c45cf3084ad5ef2ba78
Aliaksei Hurynovich <aliaksei_hurynovich@epam.com>
2018-02-27 14:13:33 +0300

    Added CORS

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ app.use((req, res, next) => {
 //Static folder settings
 //---No public folder---
 
+//CORS Middleware
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 //Bodyparser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
